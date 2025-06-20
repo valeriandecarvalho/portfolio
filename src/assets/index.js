c4cb34ab4c78f05207efa8da4cdcb52862d74813
Valerian <valeriandc.pro@gmail.com>
2025-06-20 11:13:33 +0200

    v0.0.1 config css assets ...

---
+++ src/assets/index.js
@@ -0,0 +1,83 @@
+import logo from "./logos/logo.svg";
+
+import arrowright from "./icons/arrow-right.svg";
+import arrowup from "./icons/arrow-up.svg";
+import close from "./icons/close.svg";
+import copy from "./icons/copy.svg";
+import copydone from "./icons/copy-done.svg";
+import menu from "./icons/menu.svg";
+import wavinghand from "./icons/wavinghand.svg";
+
+import github from "./logos/company/github.svg";
+import linkedin from "./logos/company/linkedin.svg";
+
+import arduino from "./logos/stack/arduino.svg";
+import cplusplus from "./logos/stack/cplusplus.svg";
+import csharp from "./logos/stack/csharp.svg";
+import css from "./logos/stack/css.png";
+import docker from "./logos/stack/docker.png";
+import figma from "./logos/stack/figma.png";
+import git from "./logos/stack/git.svg";
+import html from "./logos/stack/html.png";
+import intellij from "./logos/stack/intellij.svg";
+import java from "./logos/stack/java.svg";
+import javascript from "./logos/stack/javascript.svg";
+import mongodb from "./logos/stack/mongodb.png";
+import nodejs from "./logos/stack/nodejs.png";
+import python from "./logos/stack/python.svg";
+import raspberry from "./logos/stack/raspberry.svg";
+import reactjs from "./logos/stack/reactjs.png";
+import redux from "./logos/stack/redux.png";
+import stackoverflow from "./logos/stack/stackoverflow.svg";
+import tailwind from "./logos/stack/tailwind.png";
+import threejs from "./logos/stack/threejs.svg";
+import typescript from "./logos/stack/typescript.png";
+import vitejs from "./logos/stack/vitejs.svg";
+import vscode from "./logos/stack/vscode.svg";
+import webstorm from "./logos/stack/webstorm.png";
+
+import aalto from "./logos/university/aalto.png";
+import ut from "./logos/university/ut.png";
+
+export {
+  logo,
+
+  arrowright,
+  arrowup,
+  close,
+  copy,
+  copydone,
+  menu,
+  wavinghand,
+
+  github,
+  linkedin,
+
+  arduino,
+  cplusplus,
+  csharp,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  intellij,
+  java,
+  javascript,
+  mongodb,
+  nodejs,
+  python,
+  raspberry,
+  reactjs,
+  redux,
+  stackoverflow,
+  tailwind,
+  threejs,
+  typescript,
+  vitejs,
+  vscode,
+  webstorm,
+
+  aalto,
+  ut,
+};
